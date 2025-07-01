aa46245b94d495c702aeec978b133514296c62c1
Luigi <luigi-anderson@hotmail.com>
2025-07-01 15:35:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core')
 
 try {
-  const meuImput = core.getInput('my-input');
+  const meuInput = core.getInput('my-input');
   console.log('Hello ${meuInput}!')
   core.setOutput ("my-output", 'Hello ${meuInput}!')
 } catch (e) {
